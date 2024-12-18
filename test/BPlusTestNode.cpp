d6b5fba237a486fbb7a21108cbe0d6c98c5f2e8c
Ruyangosaurus <anzu6131@gmail.com>
2024-12-18 16:04:52 +0200

    Update BPlusTestNode.cpp
    Changed to accommodate the structural redesign of BPlusNode.

--- test/BPlusTestNode.cpp
+++ test/BPlusTestNode.cpp
@@ -1,4 +1,4 @@
-#include "BPlusTestNode.hpp"
+#include "../src/BPlusNode.hpp"
 #include <cassert>
 #include <iostream>
 #include <functional>
@@ -14,17 +14,17 @@ void tester(const char* const name, std::function<const char* const ()> func) {
 }
 
 int main(){
-    std::cout << "BPlusLeafNode TESTS:\n";
+    std::cout << "BPlusNode TESTS:\n";
 
     tester("1. empty leaf", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
-        _ASSERT(even_leaf.get_key_num() == 0);
-        _ASSERT(odd_leaf.get_key_num() == 0);
-        _ASSERT(even_leaf.get_next() == nullptr);
-        _ASSERT(odd_leaf.get_next() == nullptr);
-        _ASSERT(even_leaf.get_prev() == nullptr);
-        _ASSERT(odd_leaf.get_prev() == nullptr);
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
+        _ASSERT(even_leaf.m_key_counter == 0);
+        _ASSERT(odd_leaf.m_key_counter == 0);
+        _ASSERT(even_leaf.m_next == nullptr);
+        _ASSERT(odd_leaf.m_next == nullptr);
+        _ASSERT(even_leaf.m_prev == nullptr);
+        _ASSERT(odd_leaf.m_prev == nullptr);
         _ASSERT(even_leaf.search(0) == nullptr);
         _ASSERT(odd_leaf.search(0) == nullptr);
         _ASSERT(even_leaf.erase(0) == false);
@@ -35,8 +35,8 @@ int main(){
     });
 
     tester("2. basic insertion", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(0,0);
         even_leaf.insert(10,10);
         even_leaf.insert(30,30);
@@ -47,8 +47,8 @@ int main(){
         odd_leaf.insert(0,0);
         odd_leaf.insert(10,10);
         
-        _ASSERT(even_leaf.get_key_num() == 4);
-        _ASSERT(odd_leaf.get_key_num() == 5);
+        _ASSERT(even_leaf.m_key_counter == 4);
+        _ASSERT(odd_leaf.m_key_counter == 5);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(even_leaf.search(10)) == 10);
@@ -60,21 +60,22 @@ int main(){
         _ASSERT(*(odd_leaf.search(40)) == 40);
         even_leaf.erase_all();
         odd_leaf.erase_all();
-        _ASSERT(even_leaf.get_key_num() == 0);
-        _ASSERT(odd_leaf.get_key_num() == 0);
+        _ASSERT(even_leaf.m_key_counter == 0);
+        _ASSERT(odd_leaf.m_key_counter == 0);
         _CONCLUDE;
     });
 
     tester("3. insertion above split", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(10,10);
         even_leaf.insert(20,20);
         even_leaf.insert(40,40);
         even_leaf.insert(0,0);
 
-        BPlusLeafNode<4, int, int>* new_even_leaf = dynamic_cast<BPlusLeafNode<4, int, int>*>(even_leaf.insert(30,30));
-        _ASSERT(even_leaf.is_split_from_me(new_even_leaf));
+        BPlusNode<4, int, int>* new_even_leaf = even_leaf.insert(30,30);
+        _ASSERT(even_leaf.m_next == new_even_leaf);
+        _ASSERT(new_even_leaf->m_prev == &even_leaf);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(even_leaf.search(10)) == 10);
         _ASSERT(((even_leaf.search(20) != nullptr) && (*(even_leaf.search(20)) == 20)) || (*(new_even_leaf->search(20)) == 20));
@@ -90,8 +91,9 @@ int main(){
         odd_leaf.insert(50,50);
         odd_leaf.insert(0,0);
 
-        BPlusLeafNode<5, int, int>* new_odd_leaf = dynamic_cast<BPlusLeafNode<5, int, int>*>(odd_leaf.insert(40,40));
-        _ASSERT(odd_leaf.is_split_from_me(new_odd_leaf));
+        BPlusNode<5, int, int>* new_odd_leaf = odd_leaf.insert(40,40);
+        _ASSERT(odd_leaf.m_next == new_odd_leaf);
+        _ASSERT(new_odd_leaf->m_prev == &odd_leaf);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(10)) == 10);
         _ASSERT(*(odd_leaf.search(20)) == 20);
@@ -105,15 +107,16 @@ int main(){
     });
 
     tester("4. insertion below split", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(30,30);
         even_leaf.insert(20,20);
         even_leaf.insert(40,40);
         even_leaf.insert(0,0);
 
-        BPlusLeafNode<4, int, int>* new_even_leaf = dynamic_cast<BPlusLeafNode<4, int, int>*>(even_leaf.insert(10,10));
-        _ASSERT(even_leaf.is_split_from_me(new_even_leaf));
+        BPlusNode<4, int, int>* new_even_leaf = even_leaf.insert(10,10);
+        _ASSERT(even_leaf.m_next == new_even_leaf);
+        _ASSERT(new_even_leaf->m_prev == &even_leaf);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(even_leaf.search(10)) == 10);
         _ASSERT(((even_leaf.search(20) != nullptr) && (*(even_leaf.search(20)) == 20)) || (*(new_even_leaf->search(20)) == 20));
@@ -129,8 +132,9 @@ int main(){
         odd_leaf.insert(50,50);
         odd_leaf.insert(0,0);
 
-        BPlusLeafNode<5, int, int>* new_odd_leaf = dynamic_cast<BPlusLeafNode<5, int, int>*>(odd_leaf.insert(10,10));
-        _ASSERT(odd_leaf.is_split_from_me(new_odd_leaf));
+        BPlusNode<5, int, int>* new_odd_leaf = odd_leaf.insert(10,10);
+        _ASSERT(odd_leaf.m_next == new_odd_leaf);
+        _ASSERT(new_odd_leaf->m_prev == &odd_leaf);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(10)) == 10);
         _ASSERT(*(odd_leaf.search(20)) == 20);
@@ -144,15 +148,16 @@ int main(){
     });
 
     tester("5. insertion near split", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(30,30);
         even_leaf.insert(10,10);
         even_leaf.insert(40,40);
         even_leaf.insert(0,0);
 
-        BPlusLeafNode<4, int, int>* new_even_leaf = dynamic_cast<BPlusLeafNode<4, int, int>*>(even_leaf.insert(20,20));
-        _ASSERT(even_leaf.is_split_from_me(new_even_leaf));
+        BPlusNode<4, int, int>* new_even_leaf = even_leaf.insert(20,20);
+        _ASSERT(even_leaf.m_next == new_even_leaf);
+        _ASSERT(new_even_leaf->m_prev == &even_leaf);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(even_leaf.search(10)) == 10);
         _ASSERT(((even_leaf.search(20) != nullptr) && (*(even_leaf.search(20)) == 20)) || (*(new_even_leaf->search(20)) == 20));
@@ -168,8 +173,9 @@ int main(){
         odd_leaf.insert(50,50);
         odd_leaf.insert(0,0);
 
-        BPlusLeafNode<5, int, int>* new_odd_leaf = dynamic_cast<BPlusLeafNode<5, int, int>*>(odd_leaf.insert(20,20));
-        _ASSERT(odd_leaf.is_split_from_me(new_odd_leaf));
+        BPlusNode<5, int, int>* new_odd_leaf = odd_leaf.insert(20,20);
+        _ASSERT(odd_leaf.m_next == new_odd_leaf);
+        _ASSERT(new_odd_leaf->m_prev == &odd_leaf);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(10)) == 10);
         _ASSERT(*(odd_leaf.search(20)) == 20);
@@ -186,8 +192,9 @@ int main(){
         odd_leaf.insert(50,50);
         odd_leaf.insert(0,0);
 
-        new_odd_leaf = dynamic_cast<BPlusLeafNode<5, int, int>*>(odd_leaf.insert(30,30));
-        _ASSERT(odd_leaf.is_split_from_me(new_odd_leaf));
+        new_odd_leaf = odd_leaf.insert(30,30);
+        _ASSERT(odd_leaf.m_next == new_odd_leaf);
+        _ASSERT(new_odd_leaf->m_prev == &odd_leaf);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(10)) == 10);
         _ASSERT(*(odd_leaf.search(20)) == 20);
@@ -201,8 +208,8 @@ int main(){
     });
 
     tester("6. duplicate insertion", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(0,0);
         odd_leaf.insert(10,10);
         
@@ -211,21 +218,21 @@ int main(){
 
         even_leaf.insert(0,20);
         odd_leaf.insert(10,30);
-        _ASSERT(even_leaf.get_key_num() == 1);
-        _ASSERT(odd_leaf.get_key_num() == 1);
+        _ASSERT(even_leaf.m_key_counter == 1);
+        _ASSERT(odd_leaf.m_key_counter == 1);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(10)) == 10);
 
         even_leaf.erase_all();
         odd_leaf.erase_all();
-        _ASSERT(even_leaf.get_key_num() == 0);
-        _ASSERT(odd_leaf.get_key_num() == 0);
+        _ASSERT(even_leaf.m_key_counter == 0);
+        _ASSERT(odd_leaf.m_key_counter == 0);
         _CONCLUDE;
     });
 
     tester("7. deletion", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf;
-        BPlusLeafTestInterface<5,int,int> odd_leaf;
+        BPlusNode<4,int,int> even_leaf (true);
+        BPlusNode<5,int,int> odd_leaf (true);
         even_leaf.insert(0,0);
         even_leaf.insert(10,10);
         even_leaf.insert(30,30);
@@ -236,8 +243,8 @@ int main(){
         odd_leaf.insert(0,0);
         odd_leaf.insert(10,10);
         
-        _ASSERT(even_leaf.get_key_num() == 4);
-        _ASSERT(odd_leaf.get_key_num() == 5);
+        _ASSERT(even_leaf.m_key_counter == 4);
+        _ASSERT(odd_leaf.m_key_counter == 5);
         _ASSERT(*(even_leaf.search(0)) == 0);
         _ASSERT(*(odd_leaf.search(0)) == 0);
         _ASSERT(*(even_leaf.search(10)) == 10);
@@ -250,8 +257,8 @@ int main(){
 
         even_leaf.erase(0);
         odd_leaf.erase(0);
-        _ASSERT(even_leaf.get_key_num() == 3);
-        _ASSERT(odd_leaf.get_key_num() == 4);
+        _ASSERT(even_leaf.m_key_counter == 3);
+        _ASSERT(odd_leaf.m_key_counter == 4);
         _ASSERT(even_leaf.search(0) == nullptr);
         _ASSERT(odd_leaf.search(0) == nullptr);
         _ASSERT(*(even_leaf.search(10)) == 10);
@@ -264,8 +271,8 @@ int main(){
 
         even_leaf.erase(10);
         odd_leaf.erase(10);
-        _ASSERT(even_leaf.get_key_num() == 2);
-        _ASSERT(odd_leaf.get_key_num() == 3);
+        _ASSERT(even_leaf.m_key_counter == 2);
+        _ASSERT(odd_leaf.m_key_counter == 3);
         _ASSERT(even_leaf.search(0) == nullptr);
         _ASSERT(odd_leaf.search(0) == nullptr);
         _ASSERT(even_leaf.search(10) == nullptr);
@@ -278,8 +285,8 @@ int main(){
 
         even_leaf.erase(30);
         odd_leaf.erase(30);
-        _ASSERT(even_leaf.get_key_num() == 1);
-        _ASSERT(odd_leaf.get_key_num() == 2);
+        _ASSERT(even_leaf.m_key_counter == 1);
+        _ASSERT(odd_leaf.m_key_counter == 2);
         _ASSERT(even_leaf.search(0) == nullptr);
         _ASSERT(odd_leaf.search(0) == nullptr);
         _ASSERT(even_leaf.search(10) == nullptr);
@@ -292,8 +299,8 @@ int main(){
 
         even_leaf.erase(20);
         odd_leaf.erase(20);
-        _ASSERT(even_leaf.get_key_num() == 0);
-        _ASSERT(odd_leaf.get_key_num() == 1);
+        _ASSERT(even_leaf.m_key_counter == 0);
+        _ASSERT(odd_leaf.m_key_counter == 1);
         _ASSERT(even_leaf.search(0) == nullptr);
         _ASSERT(odd_leaf.search(0) == nullptr);
         _ASSERT(even_leaf.search(10) == nullptr);
@@ -306,42 +313,44 @@ int main(){
 
         even_leaf.erase_all();
         odd_leaf.erase_all();
-        _ASSERT(even_leaf.get_key_num() == 0);
-        _ASSERT(odd_leaf.get_key_num() == 0);
+        _ASSERT(even_leaf.m_key_counter == 0);
+        _ASSERT(odd_leaf.m_key_counter == 0);
         _CONCLUDE;
     });
 
     std::cout << "BPlusInternalNode TESTS:\n";
     
     tester("8. search pt. 1", []{
-        BPlusLeafTestInterface<4,int,int> even_leaf_1;
+        BPlusNode<4,int,int> even_leaf_1 (true);
         even_leaf_1.insert(0,0);
         even_leaf_1.insert(10,10);
         even_leaf_1.insert(30,30);
         even_leaf_1.insert(20,20);
-        BPlusLeafTestInterface<4,int,int> even_leaf_2;
+        BPlusNode<4,int,int> even_leaf_2 (true);
         even_leaf_2.insert(40,40);
         even_leaf_2.insert(50,50);
         even_leaf_2.insert(60,60);
         even_leaf_2.insert(70,70);
-        BPlusInternalNodeTestInterface<4,int,int> even_node (&even_leaf_1);
-        even_node.push_child(&even_leaf_2);
+        BPlusNode<4,int,int> even_node (false);
+        even_node.m_key_counter = 2;
+        std::get<1>(even_node.m_data)[0] = &even_leaf_1;
+        std::get<1>(even_node.m_data)[1] = &even_leaf_2;
+        even_node.m_keys[0] = even_leaf_1.m_keys[0];
+        even_node.m_keys[1] = even_leaf_2.m_keys[0];
 
-        _ASSERT(even_node.get_key_num() == 1);
-        _ASSERT(even_node.get_key(0) == 40);
-        _ASSERT(even_node.min_key() == 0);
-        _ASSERT(even_node.max_key() == 70);
-        _ASSERT(even_node.max_key() == 70);
-        _ASSERT(even_node.get_child(0) == &even_leaf_1);
-        _ASSERT(even_node.get_child(1) == &even_leaf_2);
-        _ASSERT(*(even_leaf.search(0)) == 0);
-        _ASSERT(*(even_leaf.search(10)) == 10);
-        _ASSERT(*(even_leaf.search(20)) == 20);
-        _ASSERT(*(even_leaf.search(30)) == 30);
-        _ASSERT(*(even_leaf.search(40)) == 40);
-        _ASSERT(*(even_leaf.search(50)) == 50);
-        _ASSERT(*(even_leaf.search(60)) == 60);
-        _ASSERT(*(even_leaf.search(70)) == 70);
+        _ASSERT(even_node.m_key_counter == 2);
+        _ASSERT(even_node.m_keys[0] == 0);
+        _ASSERT(even_node.m_keys[1] == 40);
+        _ASSERT(std::get<1>(even_node.m_data)[0] == &even_leaf_1);
+        _ASSERT(std::get<1>(even_node.m_data)[1] == &even_leaf_2);
+        _ASSERT(*(even_node.search(0)) == 0);
+        _ASSERT(*(even_node.search(10)) == 10);
+        _ASSERT(*(even_node.search(20)) == 20);
+        _ASSERT(*(even_node.search(30)) == 30);
+        _ASSERT(*(even_node.search(40)) == 40);
+        _ASSERT(*(even_node.search(50)) == 50);
+        _ASSERT(*(even_node.search(60)) == 60);
+        _ASSERT(*(even_node.search(70)) == 70);
         even_leaf_1.erase_all();
         even_leaf_2.erase_all();
         _CONCLUDE;
