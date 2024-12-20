697c075e23290a54073fbf61162e5ff5dda1dfa6
Ruyangosaurus <anzu6131@gmail.com>
2024-12-20 23:09:07 +0200

    Update BPlusTest.cpp
    typo + testing CRUD operations

--- test/BPlusTest.cpp
+++ test/BPlusTest.cpp
@@ -1,4 +1,4 @@
-#include "BPLusTest.hpp"
+#include "BPlusTest.hpp"
 
 void BPlusTest::tester(const char* const name, std::function<const char* const ()> func) {
     std::cout << name << func();
@@ -8,4 +8,6 @@ void BPlusTest::operator()()
 {
     std::cout << "TESTING INTERNAL NODE STRUCTURE\n";
     test_nodes();
+    std::cout << "TESTING INSERT, ERASE, AT\n";
+    test_crud();
 }
