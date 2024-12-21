be840035331ee1b085e50253d50a48d67a537b04
Ruyangosaurus <anzu6131@gmail.com>
2024-12-21 23:30:50 +0200

    Update BPlusCRUDTest.cpp
    template reorder

--- test/BPlusCRUDTest.cpp
+++ test/BPlusCRUDTest.cpp
@@ -2,7 +2,7 @@
 
 void BPlusTest::test_crud(){
     tester("1. rising order", []{
-        BPlusTree<3,int,int> tree;
+        BPlusTree<int,int,3> tree;
         for (int i = 0; i < 24; ++i){
             tree.insert(i,i);
             for (int j = 0; j <= i; ++j){
@@ -20,7 +20,7 @@ void BPlusTest::test_crud(){
     });
 
     tester("2. falling order", []{
-        BPlusTree<3,int,int> tree;
+        BPlusTree<int,int,3> tree;
         for (int i = 23; i >= 0; --i){
             tree.insert(i,i);
             for (int j = 23; j >= i; --j){
@@ -39,7 +39,7 @@ void BPlusTest::test_crud(){
 
     tester("3. zigzag", []{
         int array [24] = {0,23,1,22,2,21,3,20,4,19,5,18,6,17,7,16,8,15,9,14,10,13,11,12};
-        BPlusTree<3,int,int> tree;
+        BPlusTree<int,int,3> tree;
         for (int i = 0; i < 24; ++i){
             tree.insert(array[i],array[i]);
             for (int j = 0; j <= i; ++j){
@@ -51,6 +51,24 @@ void BPlusTest::test_crud(){
             tree.erase(array[i]);
             for (int j = i + 1; j < 24; ++j){
                 _ASSERT(tree.at(array[j]) == array[j]);
+            }
+        }
+        return passed;
+    });
+
+    tester("4. steps of seven", []{ // 7 and 24 are coprime
+        BPlusTree<int,int,3> tree;
+        for (int i = 0; i < 24; ++i){
+            tree.insert((i*7)%24,i);
+            for (int j = 0; j <= i; ++j){
+                _ASSERT(tree.at((j*7)%24) == j);
+            }
+        }
+
+        for (int i = 0; i < 24; ++i){
+            tree.erase((i*7)%24);
+            for (int j = i + 1; j < 24; ++j){
+                _ASSERT(tree.at((j*7)%24) == j);
             }
         }
         return passed;
