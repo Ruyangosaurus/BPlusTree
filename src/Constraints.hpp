b01fa00a863b3f25d0f1ae88768a62bcd2881d4f
Ruyangosaurus <anzu6131@gmail.com>
2025-01-20 11:25:20 +0200

    Update Constraints.hpp

--- src/Constraints.hpp
+++ src/Constraints.hpp
@@ -6,32 +6,49 @@
 /// @brief A concept that ensures a type is move-constructible, copy-constructible, and destructible.
 ///
 /// @requirements:
-/// * `T` must be move-constructible (`std::move_constructible<T>`).
-/// * `T` must be copy-constructible (`std::copy_constructible<T>`).
-/// * `T` must be destructible (`std::destructible<T>`).
+/// * `T` must be move-constructible with no exceptions.
+/// * `T` must be move-assignable with no exceptions.
+/// * `T` must be destructible.
 template<typename T>
-concept Storable = std::move_constructible<T> && std::copy_constructible<T> && std::destructible<T>;
+concept Storable = std::destructible<T> && requires (){
+    std::is_nothrow_move_constructible<T>::value && std::is_nothrow_move_assignable<T>::value;
+    };
 
 /// @brief A concept that ensures a type is both storable and totally ordered.
 ///
 /// @requirements:
-/// * `Key` must be move-constructible (`std::move_constructible<Key>`).
-/// * `Key` must be copy-constructible (`std::copy_constructible<Key>`).
-/// * `Key` must be destructible (`std::destructible<Key>`).
-/// * `Key` must supports the comparison operations '<' and '==' (`std::totally_ordered<Key>`).
+/// * `T` must be move-constructible with no exceptions.
+/// * `T` must be move-assignable with no exceptions.
+/// * `T` must be destructible.
+/// * `Key` must supports the comparison operations '<' and '=='.
 template<typename Key>
 concept OrderedKey = Storable<Key> && std::totally_ordered<Key>;
 
+/// @brief A concept that ensures a type has a method named allocate.
+///
+/// @requirements:
+/// * `T` must have a method named allocate that takes no arguments and returns a pointer.
 template<class Allocator>
 concept has_allocate = requires (Allocator a){
     {a.allocate()} -> std::same_as<typename Allocator::pointer>;
 };
 
+
+/// @brief A concept that ensures a type has a method named deallocate.
+///
+/// @requirements:
+/// * `T` must have a method named deallocate that takes a pointer.
 template<class Allocator>
 concept has_deallocate = requires (Allocator a, typename Allocator::pointer p){
     {a.deallocate(p)};
 };
 
+
+/// @brief A concept that ensures a type is fit to be a single element allocator.
+///
+/// @requirements:
+/// * `T` must have a method named allocate that takes no arguments and returns a pointer.
+/// * `T` must have a method named deallocate that takes a pointer.
 template<typename Alloc>
 concept SingleElementAllocator = has_deallocate<Alloc> && has_allocate<Alloc>;
 
